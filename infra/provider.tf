fd9c946b3fddd92e728d366ec7e5feb0207e4745
Arunjith S <arunnair3177@gmail.com>
2025-10-26 18:17:59 +0530

    Added the infra and the pipeline code for aks deployment

---
+++ infra/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ABB-DEMO"
+    storage_account_name = "remotebackendabb"
+    container_name       = "statefilesabb"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {}
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  type    = string
+  default = "ABB-DEMO"
+}
+variable "location" {
+  default = "eastus"
+}
+variable "aks_cluster_name" {
+  default = "ABB_Demo_Cluster"
+}
+
